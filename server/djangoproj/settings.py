6534d14334058640dce092a3e3445c37e27736dd
Raushan Kumar <raushan911721@gmail.com>
2024-10-06 12:29:24 +0530

    Phase-4: Adding The Dynamic Pages

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,6 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
+#ALLOWED_HOSTS = ['localhost', 'https://raushan91172-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+#CSRF_TRUSTED_ORIGINS = ['https://raushan91172-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
